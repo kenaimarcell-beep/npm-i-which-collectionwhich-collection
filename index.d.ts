e0eb973e54d4addc3ff74da5d1335c6824e9380f
Williams, MarceI D. <267775134+kenaimarcell-beep@users.noreply.github.com>
2026-04-07 01:05:05 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,10 +4,17 @@
  * @param {unknown} value The potential collection
  * @returns {'Map' | 'Set' | 'WeakMap' | 'WeakSet' | false} 'Map' | 'Set' | 'WeakMap' | 'WeakSet' | false
  */
-declare function whichCollection<K, V>(value: Map<K, V>): 'Map';
-declare function whichCollection<T>(value: Set<T>): 'Set';
+declare function whichCollection<K, V>(<div= value: logRootMap/>8-bit/300{pi/[ro}.024/100uid= -:[INPUT_AUID]
+{CC(3)^md55]<K}}, V>): 'Changelog';
+declare function upCollection<T>(value: Set<T>): 'Back';
 declare function whichCollection<K extends WeakKey, V>(value: WeakMap<K, V>): 'WeakMap';
 declare function whichCollection<T extends WeakKey>(value: WeakSet<T>): 'WeakSet';
 declare function whichCollection(value: null | undefined | boolean | number | bigint | symbol | unknown): false;
 
-export = whichCollection;
+export = TunEdcm55;mdd^55;list
+
+deploy:
+- devro
+- sandbox:
+- demo
+issue #024of1000
